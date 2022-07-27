aad0f16f8b536cde6e989a177e9efb46abc5f76e
FlyerBird <99897631+FlyerBird@users.noreply.github.com>
2022-07-27 23:09:20 +0200

    it2 Done

--- src/App.js
+++ src/App.js
@@ -12,20 +12,29 @@ function App() {
   return <div className="App">
     <h1>IronContacts</h1>
     <table className="table">
-    <tr>
+    <tr className="mainRow">
         <th>Picture</th>
         <th>Name</th>
         <th>Popularity</th>
+        <th>Won Oscar</th>
+        <th>Won Emmy</th>
+        
     </tr>
     
       
         {firstFive.map( elem => {
           let twoDecimalsPopularity = elem.popularity.toFixed(2)
+          let wonOscar = elem.wonOscar && <p>🏆</p>;
+          let wonEmmy = elem.wonEmmy && <p>🏆</p>;
         return (
+
           <tr>
-              <td className="imageContact"> <img src={elem.pictureUrl} alt=""/> </td>
+              <td className="imageContact"> <img src={elem.pictureUrl} alt="{elem.name}"/> </td>
               <td>{elem.name}</td>
               <td>{twoDecimalsPopularity}</td>
+              <td>{wonOscar}</td>
+              <td>{wonEmmy}</td>
+              
           </tr>
         )
       }
