4c604682fbbfd74417c5d808c0881611d25f7bde
FlyerBird <99897631+FlyerBird@users.noreply.github.com>
2022-07-28 13:35:27 +0200

    it3 and 4 Done *warningKey

--- src/App.js
+++ src/App.js
@@ -5,13 +5,40 @@ import contactsData from "./contacts.json";
 import React, {useState} from 'react';
 
 function App() {
-  const firstFive = contactsData.slice(0, 5);
+  const firstFive = [...contactsData].slice(0, 5);
   const [contacts, setContacts] = useState(firstFive);
+  const remainingContacts = contactsData.slice(5);
+
+  
+  const randomSelectContact = () => {
+      let randomContact = remainingContacts[Math.floor(Math.random()*remainingContacts.length)];
+      const newArr = [...contacts];
+      newArr.push(randomContact);
+      setContacts(newArr)  
+      console.log(newArr)
+  }
+
+  const sortByName = () => {
+    const ordered = [...contacts].sort((a,b) => a.name.localeCompare(b.name));
+    setContacts(ordered);
+    
+}
+
+
+  const sortByPopularity = () => {
+      const ordered = [...contacts].sort((a,b) => b.popularity - a.popularity);
+      setContacts(ordered);
+      
+  }
+
  
   
-
   return <div className="App">
     <h1>IronContacts</h1>
+
+    <button onClick={randomSelectContact}> Add random contact</button>
+    <button onClick={sortByName}> Sort by name</button>
+    <button onClick={sortByPopularity}> Sort by popularity</button>
 
     <table className="table">
       <thead className="mainRow">
@@ -25,14 +52,14 @@ function App() {
       </thead>
   
       <tbody>
-        {firstFive.map( elem => {
+        {contacts.map( elem => {
           let twoDecimalsPopularity = elem.popularity.toFixed(2)
           let wonOscar = elem.wonOscar && <p>🏆</p>;
           let wonEmmy = elem.wonEmmy && <p>🏆</p>;
         return (
 
        
-            <tr>
+            <tr key={elem.id}>
               <td className="imageContact"> <img src={elem.pictureUrl} alt="{elem.name}"/> </td>
               <td>{elem.name}</td>
               <td>{twoDecimalsPopularity}</td>
